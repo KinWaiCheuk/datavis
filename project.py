bb66763bc40cfaf24a30a499e917925e4d969ada
KinWaiCheuk <u3500684@connect.hku.hk>
2019-04-13 15:04:06 +0800

    Added colored clusters, but wrong images shown

--- project.py
+++ project.py
@@ -15,24 +15,32 @@ def encode_image(image_file):
     encoded = base64.b64encode(open(image_file, 'rb').read())
     return 'data:image/png;base64,{}'.format(encoded.decode())
 
+def label_filter(var, value):
+    filter_index = df['label'] == value
+    return df[var][filter_index]
+
+tracer_list = []
+for data_class in sorted(df["label"].unique()):
+    tracer = go.Scatter(
+        x = label_filter('x',data_class),
+        y = label_filter('y',data_class),
+        dy = 1,
+        mode = 'markers',
+        marker = {
+            'size': 12,
+            #'color': 'rgb(51,204,153)',
+            'line': {'width': 2}
+            },
+        name=str(data_class)
+    )
+    tracer_list.append(tracer)
+
 app.layout = html.Div([
                 html.Div([
                     dcc.Graph(
                         id='wheels-plot',
                         figure={
-                            'data': [
-                                go.Scatter(
-                                    x = df['x'],
-                                    y = df['y'],
-                                    dy = 1,
-                                    mode = 'markers',
-                                    marker = {
-                                        'size': 12,
-                                        'color': 'rgb(51,204,153)',
-                                        'line': {'width': 2}
-                                        }
-                                )
-                                    ],
+                            'data': tracer_list,
                             'layout': 
                                 go.Layout(
                                     title = 'TSN-plot',
